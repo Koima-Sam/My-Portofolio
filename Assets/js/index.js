db34ab0d5e32123d9338fdea3a874a27b814aa59
Koima-Sam <sam17koima@gmail.com>
2022-06-06 15:32:24 +0300

    check if inputs fields are empty

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,4 +1,27 @@
-const submitBtn = document.getElementById("Send")
+let submitBtn = document.getElementById("Send");
+let fname = document.getElementById("name");
+let phone = document.getElementById("phone");
+let email = document.getElementById("email");
+let subject = document.getElementById("subject");
 submitBtn.addEventListener("click", function(){
-    alert("Nothing to send")
+    if(fname.value ===""){
+        alert("name is required")
+        fname.focus();
+    }
+    else if (phone.value===""){
+        alert("phone is required");
+        phone.focus();
+    }
+    else if(email.value ===""){
+        alert("Email is required");
+        email.focus();
+    }
+    else if(subject===""){
+        alert("Subject are is empty")
+    }
+    else{
+        alert("Message sent successfully")
+        
+
+    }
 })
